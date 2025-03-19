963113e2c5f9ad80e2ae50875597498596aa84db
mohd mubeen <mohdmubeen5042@gmail.com>
2025-03-19 15:59:04 +0530

    initial commit

---
+++ app/(marketing)/_components/heading.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { Button } from "@/components/ui/button"
+import { ArrowRight } from "lucide-react"
+
+export default function Heading() {
+    return (
+        <div className="max-w-3xl space-y-5">
+            <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+                Welcome to <span className="underline">Notion</span>
+            </h1>
+            <h3 className="text-base sm:text-xl md:text-2xl font-medium">Notion is the connected workspace where  <br/> better , faster work happens</h3>
+            <Button>
+                Notion
+                <ArrowRight />
+            </Button>
+        </div>
+    )
+}
---
+++ app/(marketing)/page.tsx
@@ -0,0 +1,13 @@
+
+import Heading from "./_components/heading";
+
+
+export default function MarketingPage() {
+  return (
+    <div className="flex flex-cols min-h-full items-center justify-center">
+      <div className="flex flex-cols md:justify-start text-center gap-y-5 px-6 pb-10">
+        <Heading />
+      </div>
+    </div>
+  );
+}
